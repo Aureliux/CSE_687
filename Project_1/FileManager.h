135a3af4faf4efddf4cbf64e8c55213ba9bd9c06
jowu58 <103350997+jowu58@users.noreply.github.com>
2022-04-24 16:48:52 -0700

    Update FileManager.h

--- Project_1/FileManager.h
+++ Project_1/FileManager.h
@@ -12,10 +12,11 @@ using namespace std::filesystem;
 class FileManager
 {
 public:
-	vector<string> opentxtfile(string pathway);
-	void createtempfile(string pathway);
-	void writetotemp(string pathway, string mappedstring);
-	void createoutputfile(string pathway);
-	void writetooutput(string pathway, string outputstring);
-	void deletetemp(string pathway);
+	vector<string> opentxtfile(string pathway); // Read all the text file in the input directory and save each line to vector.
+	void createtempfile(string pathway); // Creat a temp.txt to be save temporary result from mapper.
+	void writetotemp(string pathway, string mappedstring); // Write to temp.txt.
+	void createoutputfile(string pathway); // Creat a output.txt to be save final result.
+	void writetooutput(string pathway, string outputstring); // Write to output.txt
+	void deletetemp(string pathway); // Delete temp.txt after final result save to output.txt
 };
+
