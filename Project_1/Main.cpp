05392568f623125d91eae3a310c20d00a303e9d1
jowu58 <103350997+jowu58@users.noreply.github.com>
2022-04-23 18:34:04 -0700

    Update Main.cpp

--- Project_1/Main.cpp
+++ Project_1/Main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include "Workflow.h"
 using namespace std;
+
+string convertpath(string path){
+{
+    while((pos=path.find_first_of('\\',pos))!=string::npos){
+        path.insert(pos, "\\");
+        pos=pos+2;
+    }
+    return path;
+}
 
 int main(void){
 	
@@ -17,7 +27,10 @@ int main(void){
 	getline(cin, temppath);
 	cout << "Please enter output file path:" << endl;
 	getline(cin, outputpath);
-
+	
+	inputpath = convertpath(inputpath);
+	temppath = convertpath(temppath);
+	outputpath = convertpath(outputpath);
 
 	workflow.workflow(inputpath, temppath, outputpath);		// Call workflow class and pass the file paths.
 
