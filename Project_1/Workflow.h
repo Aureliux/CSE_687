51e4e5e73ef1ceb9532038b0a7e1b54d4e37036e
jowu58 <103350997+jowu58@users.noreply.github.com>
2022-04-30 14:45:41 -0700

    update

--- Project_1/Workflow.h
+++ Project_1/Workflow.h
@@ -10,6 +10,6 @@ class Workflow
 {
 public:
 	
-	void workflow(string, string, string); /* Calls the FileManager class to handle file operations and the Mapper,
-										   Sorter, and Reducer classes to handle the modification algorithms. */
+	void workflow(string inputpath, string temppath, string outputpath); /* Calls the FileManager class to handle file operations and the Mapper,
+																			Sorter, and Reducer classes to handle the modification algorithms. */
 };
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <istream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace Project1Test
+{
+
+	TEST_CLASS(Project1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::in >> 
+			Assert::
+		}
+	};
+}
