9ee4bf80a21eb7787d5364b77e737a0a9d9dddc2
Omar Vargas <53673576+Aureliux@users.noreply.github.com>
2022-05-10 17:48:11 -0700

    Update Workflow.cpp
    Merging branches.

--- Project_2/Workflow.cpp
+++ Project_2/Workflow.cpp
@@ -5,71 +5,81 @@
 //Omar Vargas, Huiying Wu
 
 #include "Workflow.h"
-#include "windows.h"
 
-typedef vector<string> (*funcmap)(string, string);
-typedef vector<string> (*funcreduce)(vector<string>);
+FileManager filemanager;
+Sorter sorter;
+
+typedef void (*funcMap)(string, string);
+typedef void (*funcLeftoverfrombuff)(string);
+typedef void (*funcReduce)(string, vector<string>);
+
+
 
 // Calls the FileManager class to handle file operations and the Mapper, Sorter, and Reducer classes to handle the modification algorithms.
 void Workflow::workflow(string inputpath, string temppath, string outputpath)
 {
-	HINSTANCE hDLL1;
-	HINSTANCE hDLL2;
-	funcmap map = nullptr;
-	funcreduce reduce = nullptr;
-	const wchar_t* libName1 = L"MAPPER_DLL";
-	const wchar_t* libName2 = L"REDUCER_DLL";
-
-	hDLL1 = LoadLibraryEx(libName1, NULL, NULL);
-	hDLL2 = LoadLibraryEx(libName2, NULL, NULL);
-
-	FileManager filemanager;
-	// Mapper mapper;
-	Sorter sorter;
-	// Reducer reducer;
-	vector<string> filetext, mappedfile, sortedtext, reducedstring, map_v;
+	vector<string> filetext, mappedfile, sortedtext, reducedstring;
 	string fileline, mappedstring;
 
-	if (hDLL1 != NULL && hDLL2 != NULL) {
-
-		// Read text files from the input directory, send each line to the mapper, and return a mapped string to the temporary directory.
-		cout << "******************" << endl;
-		cout << "Reading Files...." << endl;
-		cout << "******************" << endl << endl;
-		filetext = filemanager.opentxtfile(inputpath);
-		filemanager.createtempfile(temppath);
-		for (int i = 0; i < filetext.size(); i++) {
-			fileline = filetext[i];
-			map_v = map(temppath, fileline);
-			filemanager.export_map(temppath, map_v);
+	// Read text files from the input directory, send each line to the mapper, and return a mapped string to the temporary directory.
+	cout << "******************" << endl;
+	cout << "Reading Files...." << endl;
+	cout << "******************" << endl << endl;
+	filetext = filemanager.opentxtfile(inputpath);
+	filemanager.createtempfile(temppath);
+	HINSTANCE hMapDLL;
+	funcMap map;
+	funcLeftoverfrombuff leftoverfrombuff;
+	const wchar_t* libName = L"MAPLIBRARY";
+	hMapDLL = LoadLibraryEx(libName, NULL, NULL);
+	if (hMapDLL != NULL) {
+		map = (funcMap)GetProcAddress(hMapDLL, "map");
+		leftoverfrombuff = (funcLeftoverfrombuff)GetProcAddress(hMapDLL, "leftoverfrombuff");
+		if (map != NULL) {
+			for (int i = 0; i < filetext.size(); i++) {
+				fileline = filetext[i];
+				map(temppath, fileline);
+			}
 		}
-		// map_v = ;
-		filemanager.leftoverfrombuff(temppath, map_v);
-
-		// Call the sorting method to read the mapped text in the temporary directory and perform an alphabetical sort.
-		filetext = filemanager.opentxtfile(temppath);
-		cout << "******************" << endl;
-		cout << "Sorting File...." << endl;
-		cout << "******************" << endl << endl;
-		sorter.sortfile(temppath);
-
-		// Read the sorted text file and send it to the reducer, which will return a reduced text string which is subsequently saved to the output directory.
-		sortedtext = filemanager.readsortedfile(temppath);
-		cout << "******************" << endl;
-		cout << "Reducing File...." << endl;
-		cout << "******************" << endl << endl;
-		reducedstring = reduce(sortedtext);
-		filemanager.createoutputfile(outputpath, "\\output.txt");
-		for (int i = 0; i < reducedstring.size(); i++) {
-			filemanager.writetooutput(outputpath, "\\output.txt", reducedstring[i]);
-		}
-
-		// Create a success file once the Map-Reduce operation is completed.
-		filemanager.createoutputfile(outputpath, "\\success.txt");
-		filemanager.writetooutput(outputpath, "\\success.txt", "SUCCESS");
-		filemanager.deletetemp(temppath);
+		if (leftoverfrombuff != NULL)
+			leftoverfrombuff(temppath);
+		FreeLibrary(hMapDLL);
 	}
 	else {
-		std::cout << "Library load failed!" << std::endl;
+		std::cout << "Map Library load failed!" << std::endl;
 	}
+
+
+	//Call the sorting method to read the mapped text in the temporary directory and perform an alphabetical sort.
+	filetext = filemanager.opentxtfile(temppath);
+	cout << "******************" << endl;
+	cout << "Sorting File...." << endl;
+	cout << "******************" << endl << endl;
+	sorter.sortfile(temppath);
+
+	// Read the sorted text file and send it to the reducer, which will return a reduced text string which is subsequently saved to the output directory.
+	sortedtext = filemanager.readsortedfile(temppath);
+	cout << "******************" << endl;
+	cout << "Reducing File...." << endl;
+	cout << "******************" << endl << endl;
+
+	filemanager.createoutputfile(outputpath, "\\output.txt");
+
+	HINSTANCE hReduceDLL;
+	funcReduce reduce;
+	const wchar_t* libName1 = L"REDUCELIBRARY";
+	hReduceDLL = LoadLibraryEx(libName1, NULL, NULL);
+	if (hReduceDLL != NULL) {
+		reduce = (funcReduce)GetProcAddress(hReduceDLL, "reduce");
+		if (reduce != NULL)
+			reduce(outputpath, sortedtext);
+		FreeLibrary(hReduceDLL);
+	}
+	else {
+		cout << "Reduce Library load failed!" << endl;
+	}
+	// Create a success file once the Map-Reduce operation is completed.
+	filemanager.createoutputfile(outputpath, "\\success.txt");
+	filemanager.writetooutput(outputpath, "\\success.txt", "SUCCESS");
+	filemanager.deletetemp(temppath);
 }
